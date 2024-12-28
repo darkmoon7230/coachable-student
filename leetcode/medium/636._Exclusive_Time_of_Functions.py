5f4fa7d11805e2bdc8f6b3a4673060674c97b55b
darkmoon7230 <dark.moon7230@fastmail.com>
2024-12-28 22:35:33 +0000

    st => prev_segments

--- leetcode/medium/636._Exclusive_Time_of_Functions.py
+++ leetcode/medium/636._Exclusive_Time_of_Functions.py
@@ -11,21 +11,21 @@
 class Solution:
     def exclusiveTime(self, n: int, logs: List[str]) -> List[int]:
         res = [0] * n
-        st = []
+        prev_segments = []
         messages = [tuple(msg.split(":")) for msg in logs]
         messages = [(int(func), state, int(end)) for func, state, end in messages]
         prev_time = 0
         for func, state, end in messages:
             duration = end - prev_time
             if state == "start":
-                if st:
-                    res[st[-1][0]] += duration
-                st.append((func, state, end))
+                if prev_segments:
+                    res[prev_segments[-1][0]] += duration
+                prev_segments.append((func, state, end))
                 prev_time = end
             else:
-                res[st[-1][0]] += duration + 1
+                res[prev_segments[-1][0]] += duration + 1
                 prev_time = end + 1
-                st.pop()
+                prev_segments.pop()
         return res
 
 
