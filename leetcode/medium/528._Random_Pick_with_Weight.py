f7cf3cae2eee4f33b75715bc770afab13a3207a9
darkmoon7230 <dark.moon7230@fastmail.com>
2024-12-31 00:50:56 +0000

    fix random pick with weight

--- leetcode/medium/528._Random_Pick_with_Weight.py
+++ leetcode/medium/528._Random_Pick_with_Weight.py
@@ -12,20 +12,28 @@
 
 
 import random
-import bisect
+
+def bisect_left(arr: List[int], target: int) -> int:
+    lo, high = 0, len(arr)
+    while lo < high:
+        mid = lo + (high - lo) // 2
+        if arr[mid] < target:
+            lo = mid + 1
+        else:
+            high = mid
+    return lo
 
 class Solution:
 
     def __init__(self, w: List[int]):
-        self._cum = []
-        acc = 0
+        self._cumulative_weight = []
+        self._total_weight = 0
         for ele in w:
-            acc += ele
-            self._cum.append(acc)
-        self._sum = acc
+            self._total_weight += ele
+            self._cumulative_weight.append(self._total_weight)
 
     def pickIndex(self) -> int:
-        val = random.randint(1, self._sum)
-        idx = bisect.bisect_left(self._cum, val)
+        val = random.randint(1, self._total_weight)
+        idx = bisect_left(self._cumulative_weight, val)
         return idx
 
