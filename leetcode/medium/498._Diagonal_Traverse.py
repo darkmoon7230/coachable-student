ddac61adb4c81193c53479f1775cc42637bbd662
darkmoon7230 <dark.moon7230@fastmail.com>
2024-12-31 21:30:42 +0000

    update diagonal traverse

--- leetcode/medium/498._Diagonal_Traverse.py
+++ leetcode/medium/498._Diagonal_Traverse.py
@@ -8,60 +8,70 @@
 # Difficulty: Medium
 # Code
 
+from enum import Enum
+
+# We build a state machine to describe the walk process
+class State(Enum):
+    UP_RIGHT = 0
+    UP_RIGHT_FIX = 1
+    DOWN_LEFT = 2
+    DOWN_LEFT_FIX = 3
+
+# Yes these vectores does tell us that up-right and down-left
+# operations are just rotated versions of each other
+UP_RIGHT_DIRECTION = (-1, 1)
+UP_RIGHT_FIX_CEIL = (1, 0)
+UP_RIGHT_FIX_SIDE = (2, -1)
+
+DOWN_LEFT_DIRECTION = (1, -1)
+DOWN_LEFT_FIX_SIDE = (0, 1)
+DOWN_LEFT_FIX_BOTTOM = (-1, 2)
+
+
 class Solution:
     def findDiagonalOrder(self, mat: List[List[int]]) -> List[int]:
-        def next_up_cord(cord) -> List[int]:
-            x, y = cord
-            return [x + 1, y - 1]
+        def piecewise_add(base: tuple[int], diff: tuple[int]) -> tuple[int]:
+            return base[0] + diff[0], base[1] + diff[1]
 
-        def next_down_cord(cord) -> List[int]:
-            x, y = cord
-            return [x - 1, y + 1]
+        def out_of_bound(cord: tuple[int]) -> bool:
+            y, x = cord
+            return not (0 <= y < len(mat)) or not (0 <= x < len(mat[0]))
 
-        def out_of_bound(cord) -> bool:
-            x, y = cord
-            if x < 0 or y < 0:
-                return True
-            if y >= len(mat):
-                return True
-            if x >= len(mat[0]):
-                return True
-            return False
-
-        def at(cord) -> int:
-            x, y = tuple(cord)
+        def at(cord: tuple[int]) -> int:
+            print(cord)
+            y, x = cord
             return mat[y][x]
 
-        goUp = True
+        state = State.UP_RIGHT
+        cord = (0, 0)
+        target = (len(mat) - 1, len(mat[0]) - 1)
         res = []
-        cord = [0, 0]
-        while cord != (len(mat[0]), len(mat)):
-            while not out_of_bound(cord):
-                res.append(at(cord))
-                cord = next_up_cord(cord) if goUp else next_down_cord(cord)
-
-            # Okay now we need to fix the next cord
-            if goUp:
-                # Usually it is on top of the next element
-                cord[1] += 1
-                # However, when its on the edge, we will need to move it in
-                if out_of_bound(cord):
-                    cord[1] += 1
-                    cord[0] -= 1
-            else:
-                # When coming out of the left edge,
-                # the element is should be on the right
-                cord[0] += 1
-                # When coming out of the bottom,
-                # we need to move it in
-                if out_of_bound(cord):
-                    cord[1] -= 1
-                    cord[0] += 1
-            goUp = not goUp
-
-            if out_of_bound(cord):
-                break
-
+        while cord != target:
+            match (state):
+                case State.UP_RIGHT:
+                    res.append(at(cord))
+                    cord = piecewise_add(cord, UP_RIGHT_DIRECTION)
+                    if out_of_bound(cord):
+                        state = State.UP_RIGHT_FIX
+                case State.UP_RIGHT_FIX:
+                    # overflow from the side
+                    if cord[1] == len(mat[0]):
+                        cord = piecewise_add(cord, UP_RIGHT_FIX_SIDE)
+                    else:
+                        cord = piecewise_add(cord, UP_RIGHT_FIX_CEIL)
+                    state = State.DOWN_LEFT
+                case State.DOWN_LEFT:
+                    res.append(at(cord))
+                    cord = piecewise_add(cord, DOWN_LEFT_DIRECTION)
+                    if out_of_bound(cord):
+                        state = State.DOWN_LEFT_FIX
+                case State.DOWN_LEFT_FIX:
+                    if cord[0] == len(mat):
+                        cord = piecewise_add(cord, DOWN_LEFT_FIX_BOTTOM)
+                    else:
+                        cord = piecewise_add(cord, DOWN_LEFT_FIX_SIDE)
+                    state = State.UP_RIGHT
+        res.append(at(cord))
         return res
 
 
